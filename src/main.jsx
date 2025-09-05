ae951313d991896684e36d16ff68280db02d592b
Daniel Limón <dlimon.munoz@gmail.com>
2025-09-05 13:30:45 -0600

    mod: vite.config.js|scr/main.jsx > Se sigue solucionando el manejo de rutas, que da problemas por React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Importa BrowserRouter y Route
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <Routes>
-        {/* Define la ruta dinámica con :id_organizacion */}
         <Route path="/encuesta/:id_organizacion" element={<App />} />
       </Routes>
     </BrowserRouter>
