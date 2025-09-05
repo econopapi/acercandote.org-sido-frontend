aba8b9b72dfed0930f28db608ca28e77027f12e8
Daniel Limón <dlimon.munoz@gmail.com>
2025-09-05 13:36:42 -0600

    mod: se siguen solucionando problemas de despliegue

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Asegura que los archivos estáticos se sirvan desde la raíz
+  base: './', // Rutas relativas para producción
 });
